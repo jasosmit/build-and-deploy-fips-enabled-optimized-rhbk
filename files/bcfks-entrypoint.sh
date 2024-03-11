b0546c9f4a88b51bd43aba6a5f9719c4699a8a9e
Jason Smith <jasosmit@redhat.com>
2024-03-10 19:35:18 -0500

    Added the sun.security.provider=ALL-UNAMED and security.util=ALL-UNAMED to resolve java issue

--- files/bcfks-entrypoint.sh
+++ files/bcfks-entrypoint.sh
@@ -7,4 +7,6 @@ keytool -importkeystore -srckeystore /certs/$SRCSTOREFILE.jks -srcstoretype JKS 
 	-provider org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider \
 	-providerpath /opt/keycloak/providers/bc-fips-1.0.2.3.jar \
 	-J-Djava.security.properties=/usr/lib/jvm/jre-17-openjdk/conf/security/java.security \
+	-J--add-exports=java.base/sun.security.provider=ALL-UNNAMED \
+        -J--add-opens=java.base/sun.security.util=ALL-UNNAMED \
 	-providername BCFIPS
